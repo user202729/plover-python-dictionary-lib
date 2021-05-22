347187aad98ab2462f2f2be57cdd2b1fbaae27e5
user202729 <25191436+user202729@users.noreply.github.com>
2021-05-22 17:27:35 +0700

    Version 0.1.0

--- example_python_dictionary.py
+++ example_python_dictionary.py
@@ -7,78 +7,119 @@ from plover.system import english_stenotype as e
 from plover_python_dictionary_lib import get_context_from_system
 
 context=get_context_from_system(e)
+s=context.SingleDictionary
+stroke=context.stroke
+translation=context.translation
 
-def merge_function(first_stroke, left, right):
-	return "{&" + left + right + "}"
+leader_stroke="TP*EURPBG" # finger*
+leader_stroke_2="TP*EURPBGS" # fingers*
 
-dictionary=context.SingleDictionary(merge_function,
-	first_stroke="TP*EURPBG", # finger*
-	stroke_separator="/",
-	left={
-		'A':       'a',
-		'PW':      'b',
-		'KR':      'c',
-		'TK':      'd',
-		#'??':     'e',
-		'TP':      'f',
-		'TKPW':    'g',
-		'H':       'h',
-		#'??':     'i',
-		'SKWR':    'j',
-		'K':       'k',
-		'HR':      'l',
-		'PH':      'm',
-		'TPH':     'n',
-		'O':       'o',
-		'P':       'p',
-		'KW':      'q',
-		'R':       'r',
-		'S':       's',
-		'T':       't',
-		#'??':     'u',
-		'SR':      'v',
-		'W':       'w',
-		'KP':      'x',
-		'KWR':     'y',
-		'STKPW':   'z',
-		},
-	right={
-		#'??':     'a',
-		'-B':      'b',
-		#'??':     'c',
-		'-D':      'd',
-		'E':       'e',
-		'-F':      'f',
-		'-G':      'g',
-		'*FD':     'h',
-		'EU':      'i',
-		'-PBLG':   'j',
-		'-BG':     'k',
-		'-L':     'l',
-		'-PL':     'm',
-		'-PB':     'n',
-		#'??':     'o',
-		'-P':      'p',
-		#'??':     'q',
-		'-R':      'r',
-		'-S':      's',
-		'-T':      't',
-		'U':       'u',
-		'-FB':     'v',
-		#'??':     'w',
-		'-BGS':    'x',
-		'-FRL':    'y',
-		'-Z':      'z',
-		},
-	)
+left_hand=s({
+	'A':       'a',
+	'PW':      'b',
+	'KR':      'c',
+	'TK':      'd',
+	#'??':     'e',
+	'TP':      'f',
+	'TKPW':    'g',
+	'H':       'h',
+	#'??':     'i',
+	'SKWR':    'j',
+	'K':       'k',
+	'HR':      'l',
+	'PH':      'm',
+	'TPH':     'n',
+	'O':       'o',
+	'P':       'p',
+	'KW':      'q',
+	'R':       'r',
+	'S':       's',
+	'T':       't',
+	#'??':     'u',
+	'SR':      'v',
+	'W':       'w',
+	'KP':      'x',
+	'KWR':     'y',
+	'STKPW':   'z',
+	})
 
-lookup=dictionary.lookup
+right_hand=s({
+	#'??':     'a',
+	'-B':      'b',
+	#'??':     'c',
+	'-D':      'd',
+	'E':       'e',
+	'-F':      'f',
+	'-G':      'g',
+	'*FD':     'h',
+	'EU':      'i',
+	'-PBLG':   'j',
+	'-BG':     'k',
+	'-L':     'l',
+	'-PL':     'm',
+	'-PB':     'n',
+	#'??':     'o',
+	'-P':      'p',
+	#'??':     'q',
+	'-R':      'r',
+	'-S':      's',
+	'-T':      't',
+	'U':       'u',
+	'-FB':     'v',
+	#'??':     'w',
+	'-BGS':    'x',
+	'-FRL':    'y',
+	'-Z':      'z',
+	})
+
+one_stroke=left_hand | right_hand | left_hand*right_hand
+
+dictionary = (
+		s({leader_stroke: "{#}", leader_stroke_2: "{#}"}) |
+		(stroke(leader_stroke) | stroke(leader_stroke_2)) / translation("{&") * one_stroke * translation("}") | 
+		stroke(leader_stroke_2) / translation("{&") * one_stroke / one_stroke * translation("}")
+		)
 
 
-# demonstration part -- not necessary in a real Python dictionary
+lookup=lambda strokes: dictionary.lookup_tuple(strokes)
+LONGEST_KEY=dictionary.longest_key
+
+
+
+
+
+# ======== demonstration part -- not necessary in a real Python dictionary
 if __name__=="__main__":
-	print(lookup("TP*EURPBG/SKWREU"))
-	print(lookup("TP*EURPBG/SKWR-L"))
-	
-	# currently not implemented
+	assert dictionary.longest_key==3
+
+	for d in [dictionary,
+			# map a part
+			stroke(leader_stroke)/((left_hand*right_hand).map(lambda x: "{&" + x + "}")),
+
+			# name a part for mapping (and to disable automatically concatenation behavior)
+			stroke(leader_stroke)/((left_hand.named("left")*right_hand.named("right")).map(lambda left, right: "{&" + left + right + "}")),
+
+			# map with keyword arguments
+			stroke(leader_stroke)/((left_hand.named("left")*right_hand.named("right")).map(lambda **kwargs: "{&" + kwargs["left"] + kwargs["right"] + "}")),
+
+			# not all parts must be named (those which don't are not accessible
+			(stroke(leader_stroke)/(left_hand.named("left")*right_hand.named("right"))).map(lambda left, right: "{&" + left + right + "}"),
+			(stroke(leader_stroke)/left_hand.named("left")*right_hand.named("right")).map(lambda left, right: "{&" + left + right + "}"),
+
+			# name weird parts
+			(stroke(leader_stroke).named("leader")/left_hand.named("left")*right_hand.named("right")).map(lambda leader, left, right: "{&" + left + right + "}"),
+			]:
+		for target, outline in [
+		("{&ji}", "TP*EURPBG/SKWREU"),
+		("{&jl}", "TP*EURPBG/SKWR-L"),
+		(None   , "TP*EURPBG/SKWR-BL"),
+		(None   , "SKWR-L"),
+		]:
+			result=d.lookup_str(outline)
+			assert result==target, (outline, result, target)
+
 	#dictionary.print_items()
+
+	assert "a" == s({"A": "a"}).lookup_str("A")
+	assert "a" == (s({"A": "a"}) | s({"-B": "b"})).lookup_str("A")
+	assert "b" == (s({"A": "a"}) | s({"-B": "b"})).lookup_str("-B")
--- plover_python_dictionary_lib.py
+++ plover_python_dictionary_lib.py
@@ -1,5 +1,6 @@
-from typing import Dict, TypeVar, Union, NamedTuple, Optional, Any, Callable, List, Iterable, Tuple
+from typing import Dict, TypeVar, Union, NamedTuple, Optional, Any, Callable, List, Iterable, Tuple, Mapping
 import functools
+import sys
 import operator
 import itertools
 from types import SimpleNamespace
@@ -20,204 +21,359 @@ stroke_separator=StrokeSeparator()
 
 LookupResult=Optional[str]
 
-StrokeT=TypeVar("StrokeT")
+#StrokeT=TypeVar("StrokeT")
 
-InputStrokeType=Union[str, StrokeT]
+InputStrokeType=Union[str, BaseStroke]
+#InputStrokeType=Union[str, StrokeT]
 
-def get_context(stroke_type: Callable[..., StrokeT])->Context:
-	Stroke: type=stroke_type
+InputStrokesType=Union[InputStrokeType, Iterable[InputStrokeType], Iterable[str]]
 
-	def subsets(stroke: StrokeT)->Iterable[StrokeT]:
-		import itertools
-		for keys in itertools.product([[], [key]] for key in stroke.keys()):
-			yield Stroke(itertools.chain(keys))
+Strokes=Tuple[BaseStroke, ...]
 
-	def to_stroke(stroke: InputStrokeType)->StrokeT:
-		# with the assertion
-		assert isinstance(stroke, str) or isinstance(stroke, Stroke)
-		return Stroke(stroke)
 
-	class PartialStrokeLookup(NamedTuple):
-		lookup: Callable[[StrokeT], LookupResult]  # must not raise
-		keys: StrokeT
-		keys_iterator: Callable[[], Iterable[StrokeT]]
+def outline_union(a: Strokes, b: Strokes)->Strokes:
+	return tuple(map(operator.or_, a, b))
 
-	def lookup_function_str(keys: InputStrokeType,
-			function: Callable[[str], LookupResult])->PartialStrokeLookup:
+def outline_union_strip_optional(a: Optional[Strokes], b: Optional[Strokes])->Strokes:
+	assert a
+	assert b
+	return outline_union(a, b)
+
+class Dictionary:
+	def __init__(self, stroke_type: type)->None:
+		self.stroke_type=stroke_type
+
+	def lookup(self, strokes: Strokes)->Any:
 		"""
-		Wraps a function that takes a string and returns the lookup result.
+		Lookup a stroke in the dictionary.
+		Must not raise a KeyError.
+		Return None if there's nothing found.
 
-		The resulting function might raise KeyError.
+		Arguments:
+			strokes: a RTF/CRE stroke string.
 		"""
-		def result_lookup(stroke: StrokeT)->LookupResult:
-			return function(stroke.rtfcre)
-		return PartialStrokeLookup(result_lookup, to_stroke(keys), lambda: subsets(keys))
+		raise NotImplementedError
 
-	def lookup_function_stroke(keys: Union[StrokeT, str],
-			function: Callable[[StrokeT], LookupResult])->PartialStrokeLookup:
+	def lookup_str(self, strokes: str)->LookupResult:
+		result=self.lookup(tuple(map(self.stroke_type, strokes.split("/"))))
+		assert result is None or isinstance(result, str)
+		return result
+
+	def lookup_tuple(self, strokes: Tuple[str])->LookupResult:
+		result=self.lookup(tuple(map(self.stroke_type, strokes)))
+		assert result is None or isinstance(result, str)
+		return result
+	
+	def items(self)->Iterable[Tuple[Strokes, Any]]:
 		"""
-		Wraps a function that takes a Stroke object and returns the lookup result.
-
-		The resulting function might raise KeyError.
+		Return all items in the dictionary.
 		"""
-		return PartialStrokeLookup(function, to_stroke(keys), lambda: subsets(keys))
+		raise NotImplementedError
 
-	def parse_lookup(arg: Any)->Union[PartialStrokeLookup, StrokeSeparator]:
-		if isinstance(arg, PartialStrokeLookup):
-			return arg
-		if isinstance(arg, str) and arg=="/":
-			return stroke_separator
-		if isinstance(arg, str):
-			arg={arg: arg}
-		if isinstance(arg, list):
-			arg={item: item for item in arg}
-		if isinstance(arg, dict):
-			assert arg
-			arg={to_stroke(key): value for key, value in arg.items()}
-			keys=functools.reduce(operator.or_, arg.keys())
-			return PartialStrokeLookup(arg.get, keys, lambda: arg.keys())
-		assert False, ("Unrecognized format", arg)
+	def items_str(self)->Iterable[Tuple[str, str]]:
+		for key, value in self.items():
+			assert isinstance(value, str)
+			yield "/".join(str(stroke) for stroke in key), value
 
-	class Dictionary:
-		def lookup(self, strokes: str)->LookupResult: # will not raise KeyError
-			"""
-			Lookup a stroke in the dictionary.
-
-			Arguments:
-				strokes: a RTF/CRE stroke string.
-			"""
-			raise NotImplementedError
-		
-		def items(self)->Iterable[Tuple[str, str]]:
-			"""
-			Return all items in the dictionary.
-			"""
-			raise NotImplementedError
-
-		def print_items(self)->None:
-			"""
-			Print all items in the dictionary in JSON format.
-			"""
-			import json
-			json.dumps(
-					dict(self.items())
-					, ensure_ascii=False, indent=0)
-
-
-	class LookupObjectReference(NamedTuple):
-		location: Union[int, str]
-		lookup_object: PartialStrokeLookup
-
-		@property
-		def is_keyword(self)->bool:
-			return isinstance(self.location, str)
-
-	class StrokeComponents(NamedTuple):
-		keys: StrokeT  # store the union of all PartialStrokeLookup keys to simplify implementation
-		components: List[LookupObjectReference]
-
-	class SingleDictionary(Dictionary):
+	def print_items(self)->None:
 		"""
+		Print all items in the dictionary in JSON format.
 		"""
+		import json
+		json.dump(dict(self.items_str()), sys.stdout, ensure_ascii=False, indent=0)
 
-		def __init__(self, merge_function, *args, **kwargs)->None:
-			self.merge_function=merge_function
-			self.args_component=[parse_lookup(arg) for arg in args]
-			self.kwargs_component={key: parse_lookup(value) for key, value in kwargs.items()}
+	def __or__(self, other: "Dictionary")->"Dictionary":
+		"""
+		Compute the union of two dictionaries.
+		"""
+		return AlternativeDictionary(self.stroke_type, [self, other])
 
-			components_: List[List[LookupObjectReference]]=[
-					list(group)
-					for is_lookup, group in itertools.groupby(
-						[
-							LookupObjectReference(index, lookup_object_or_separator) # type: ignore
-							for index, lookup_object_or_separator in enumerate(self.args_component)] +
-						[
-							LookupObjectReference(key, lookup_object_or_separator) # type: ignore
-							for key, lookup_object_or_separator in self.kwargs_component.items()]
-						, key=lambda x: isinstance(x.lookup_object, PartialStrokeLookup)
-						)
-					if is_lookup]
+	def __mul__(self, other: "Dictionary")->"Dictionary":
+		"""
+		Compute the (Cartesian) product of two dictionaries. The 2 adjacent strokes are merged.
+		"""
+		return ProductDictionary(self.stroke_type, self, other, merge=True)
 
-			self.components: List[StrokeComponents]=[]
-			for stroke_components in components_:
-				keys=Stroke()
-				for lookup_object_reference in stroke_components:
-					new_keys: StrokeT=lookup_object_reference.lookup_object.keys
-					assert not (keys&new_keys)
-					keys|=new_keys
-				self.components.append(StrokeComponents(keys, stroke_components))
+	def __truediv__(self, other: "Dictionary")->"Dictionary":
+		"""
+		Compute the (Cartesian) product of two dictionaries. The 2 adjacent strokes are not merged.
+		"""
+		return ProductDictionary(self.stroke_type, self, other, merge=False)
 
-		def lookup(self, strokes: str)->LookupResult:
-			strokes_1=strokes.split("/")
-			if len(strokes_1)!=len(self.components):
-				return None
-			strokes_: List[StrokeT]=[Stroke(stroke) for stroke in strokes_1]
-			for stroke, stroke_components in zip(strokes_, self.components):
-				if stroke not in stroke_components.keys:
-					return None
+	def map(self, function: Callable[..., Any])->"Dictionary":
+		"""
+		Map a function over the dictionary values.
+		"""
+		import inspect
+		argspec=inspect.getfullargspec(function)
+		assert not argspec.varargs
 
-			merge_args: List[Any]=[]  # anything that the component functions results
-			merge_kwargs: Dict[str, Any]={}
-			for stroke, stroke_components in zip(strokes_, self.components):
-				for lookup_object_reference in stroke_components.components:
-					component: StrokeT=stroke&lookup_object_reference.lookup_object.keys
-					result=lookup_object_reference.lookup_object.lookup(component)
-					if lookup_object_reference.is_keyword:
-						assert isinstance(lookup_object_reference.location, str)
-						assert lookup_object_reference.location not in merge_kwargs
-						merge_kwargs[lookup_object_reference.location]=result
-					else:
-						assert len(merge_args)==lookup_object_reference.location
-						merge_args.append(result)
+		return MappedDictionary(self.stroke_type, self, function)
 
-			try:
-				return self.merge_function(*merge_args, **merge_kwargs)
-			except KeyError:
-				return None
+	def named(self, name: str)->"NamedDictionary":
+		return NamedDictionary(self.stroke_type, self, name)
 
-		def __or__(self, other: Dictionary)->"MultipleDictionary":
-			return MultipleDictionary(self)|other
+	outline_length: Optional[int]
+	"""
+	The length of the outlines in the dictionary, or None if the information is unknown.
+	"""
 
-		def __items__(self)->Iterable[Tuple[str, str]]:
-			raise NotImplementedError
+	longest_key: int
+	"""
+	The longest_key in the dictionary.
+	"""
 
-	class MultipleDictionary(Dictionary):
-		def __init__(self, arg: Any)->None:
-			self.dictionaries: List[SingleDictionary]=[]
-			if isinstance(arg, SingleDictionary):
-				self.dictionaries=[arg]
-			elif isinstance(arg, MultipleDictionary):
-				self.dictionaries=list(arg.dictionaries)
-			else:
-				assert isinstance(arg, list)
-				for dictionary in arg:
-					assert isinstance(dictionary, SingleDictionary)
-				self.dictionaries=arg
+	outline_mask: Optional[Strokes]
+	"""
+	The union mask of the outlines in the dictionary, or None if the information is unknown.
+	"""
 
-		def lookup(self, strokes: str)->LookupResult:
-			for dictionary in self.dictionaries:
-				result=dictionary.lookup(strokes)
-				if result is not None:
-					return result
-			return None
 
-		def __or__(self, other: Dictionary)->"MultipleDictionary":
-			if isinstance(other, SingleDictionary):
-				other=MultipleDictionary(other)
-			return MultipleDictionary(self.dictionaries+other.dictionaries)
+def to_stroke(stroke_type: type, stroke: InputStrokeType)->BaseStroke:
+	# with the assertion
+	assert isinstance(stroke, str) or isinstance(stroke, stroke_type)
+	return stroke_type(stroke)
 
-		def __items__(self)->Iterable[Tuple[str, str]]:
-			return itertools.chain.from_iterable(
-					dictionary.items()
-					for dictionary in self.dictionaries
+
+def to_strokes(stroke_type: type, strokes: InputStrokesType)->Strokes:
+	if isinstance(strokes, str):
+		return (stroke_type(strokes),)
+	elif isinstance(strokes, BaseStroke):
+		return (strokes,)
+	elif isinstance(strokes, Iterable): # must handle str case before this one
+		return tuple(to_stroke(stroke_type, stroke) for stroke in strokes)
+	else:
+		assert False
+
+
+class CompoundResult(NamedTuple):
+	"""
+	Represent the lookup result of a product of dictionaries, or similar.
+	"""
+	data: Dict[str, Any]
+
+
+class NamedDictionary(Dictionary):
+	"""
+	Represent a dictionary with an assigned name, for mapping.
+	"""
+	# note that keys after used once in map cannot be used in upper map layers.
+
+	def __init__(self, stroke_type: type, wrapped: Dictionary, name: str)->None:
+		super().__init__(stroke_type)
+		assert not isinstance(wrapped, NamedDictionary)
+		self.wrapped=wrapped
+		self.name=name
+		self.longest_key=wrapped.longest_key
+		self.outline_length=wrapped.outline_length
+		self.outline_mask=wrapped.outline_mask
+
+	def name_result(self, result: Any)->CompoundResult:
+		assert not isinstance(result, CompoundResult)
+		return CompoundResult({self.name: result})
+
+	def lookup(self, strokes: Strokes)->Any:
+		result=self.wrapped.lookup(strokes)
+		if result is None: return None
+		return self.name_result(result)
+
+	def items(self)->Iterable[Tuple[Strokes, Any]]:
+		for strokes, value in self.wrapped.items():
+			yield strokes, self.name_result(value)
+
+
+
+class SingleDictionary(Dictionary):
+	def __init__(self, stroke_type: type, data: Union[Iterable[InputStrokesType], Dict[InputStrokesType, Any]])->None:
+		super().__init__(stroke_type)
+		self.data: Dict[Strokes, Any]
+		if isinstance(data, Mapping):
+			self.data={to_strokes(stroke_type, key): value for key, value in data.items()}
+		elif isinstance(data, Iterable):
+			self.data={to_strokes(stroke_type, strokes): strokes for strokes in data}
+		else:
+			assert False
+
+		assert self.data
+		self.outline_length=len(next(iter(self.data.keys())))
+		self.longest_key=max(len(strokes) for strokes in self.data.keys())
+		if any(len(strokes)!=self.outline_length for strokes in self.data.keys()):
+			self.outline_length=None
+		else:
+			self.outline_mask=functools.reduce(
+					outline_union,
+					self.data.keys()
 					)
 
+	def lookup(self, strokes: Strokes)->Any:
+		return self.data.get(strokes)
 
+	def items(self)->Iterable[Tuple[Strokes, Any]]:
+		return self.data.items()
+
+
+class ProductDictionary(Dictionary):
+	def __init__(self, stroke_type: type, a: Dictionary, b: Dictionary, merge: bool)->None:
+		super().__init__(stroke_type)
+		self.a=a
+		self.b=b
+		self.merge=merge
+		assert a.outline_length
+		assert b.outline_length
+		assert a.outline_mask
+		assert b.outline_mask
+		self.outline_length=a.outline_length+b.outline_length-merge
+		self.longest_key=a.longest_key+b.longest_key-merge
+		if merge:
+			x=a.outline_mask[-1]
+			y=b.outline_mask[0]
+			assert not (x&y)
+			self.outline_mask=a.outline_mask[:-1]+(x|y,)+b.outline_mask[1:]
+		else:
+			self.outline_mask=a.outline_mask+b.outline_mask
+
+	def lookup(self, strokes: Strokes)->Any:
+		if len(strokes)!=self.outline_length: return None
+		assert self.outline_mask
+		if any(a not in b for a, b in zip(strokes, self.outline_mask)): return None
+		a, b=self.a, self.b
+		assert a.outline_mask
+		assert b.outline_mask
+		if self.merge:
+			before=strokes[:len(a.outline_mask)-1]
+			common=strokes[len(a.outline_mask)-1]
+			after=strokes[len(a.outline_mask):]
+			strokes_a=before+(common&a.outline_mask[-1],)
+			strokes_b=(common&b.outline_mask[0],)+after
+		else:
+			strokes_a=strokes[:len(a.outline_mask)]
+			strokes_b=strokes[len(a.outline_mask):]
+
+		value_a=a.lookup(strokes_a)
+		if value_a is None: return None
+		value_b=b.lookup(strokes_b)
+		if value_b is None: return None
+
+		return self.merge_value(value_a, value_b)
+
+	def merge_stroke(self, strokes_a: Strokes, strokes_b: Strokes)->Strokes:
+		if self.merge:
+			assert (strokes_a[-1]+strokes_b[0])==(strokes_a[-1]|strokes_b[0])
+			return strokes_a[:-1]+(strokes_a[-1]+strokes_b[0],)+strokes_b[1:]
+		else:
+			return strokes_a+strokes_b
+
+	def merge_value(self, value_a: Any, value_b: Any)->Union[str, CompoundResult]:
+		if isinstance(value_a, str) and isinstance(value_b, str):
+			return value_a+value_b
+		result=CompoundResult({})
+		if isinstance(value_a, CompoundResult): result.data.update(value_a.data)
+		if isinstance(value_b, CompoundResult): result.data.update(value_b.data)
+		return result
+
+	def items(self)->Iterable[Tuple[Strokes, Any]]:
+		for strokes_a, value_a in self.a.items():
+			for strokes_b, value_b in self.b.items():
+				yield self.merge_stroke(strokes_a, strokes_b), self.merge_value(value_a, value_b)
+
+
+
+class MappedDictionary(Dictionary):
+	def __init__(self, stroke_type: type, wrapped: Dictionary, function: Callable[..., Any])->None:
+		super().__init__(stroke_type)
+		self.function=function
+		self.wrapped=wrapped
+		self.longest_key=wrapped.longest_key
+		self.outline_length=wrapped.outline_length
+		self.outline_mask=wrapped.outline_mask
+
+	def apply_function(self, strokes: Strokes, result: Any)->Any:
+
+		#include_strokes=argspec.varkw is not None or "strokes" in argspec.args:
+		#include_strokes_rtfcre=argspec.varkw is not None or "strokes_rtfcre" in argspec.args:
+		#arguments={}
+		#if include_strokes: arguments["strokes"]=strokes
+		#if include_strokes_rtfcre: arguments["strokes_rtfcre"]=strokes_rtfcre
+
+		if isinstance(result, CompoundResult):
+			return self.function(**result.data)
+		else:
+			return self.function(result)
+
+	def lookup(self, strokes: Strokes)->Any:
+		result=self.wrapped.lookup(strokes)
+		if result is None:
+			return None
+		return self.apply_function(strokes, result)
+
+	def items(self)->Iterable[Tuple[Strokes, Any]]:
+		for strokes, value in self.wrapped.items():
+			yield strokes, self.apply_function(strokes, value)
+
+
+
+class AlternativeDictionary(Dictionary):
+	"""
+	A dictionary formed as an alternative of multiple dictionaries.
+	"""
+	def __init__(self, stroke_type: type, components: List[Dictionary])->None:
+		super().__init__(stroke_type)
+		self._components: List[Dictionary]=[]
+		for component in components:
+			if isinstance(component, AlternativeDictionary):
+				self._components.extend(component._components)
+			else:
+				self._components.append(component)
+
+		assert self._components
+
+		self.longest_key=max(component.longest_key for component in self._components)
+
+		if any(component.outline_length is None
+				or component.outline_length!=self._components[0].outline_length
+				for component in self._components):
+			self.outline_length=None
+			self.outline_mask=None
+		else:
+			self.outline_length=self._components[0].outline_length
+			self.outline_mask=functools.reduce(
+					outline_union_strip_optional,
+					(component.outline_mask for component in self._components)
+					)
+
+	def items(self)->Iterable[Tuple[Strokes, Any]]:
+		for component in self._components:
+			yield from component.items()
+
+	def lookup(self, strokes: Strokes)->Any:
+		for component in self._components:
+			result=component.lookup(strokes)
+			if result is not None: return result
+
+
+def stroke(stroke_type: type, strokes: str)->Dictionary:
+	"""
+	Return a dictionary that has <strokes> as the stroke and nothing as the translation.
+	Useful as part of a dictionary.
+
+	The current implementation is not very efficient.
+	"""
+	return SingleDictionary(stroke_type, {strokes: ""})
+
+def translation(stroke_type: type, translation: str)->Dictionary:
+	"""
+	Return a dictionary that has <translation> as the translation and nothing as the stroke.
+	Useful as part of a dictionary.
+
+	The current implementation is not very efficient.
+	"""
+	return SingleDictionary(stroke_type, {"": translation})
+
+def get_context(stroke_type: type)->Context:
 	return Context(
-			lookup_function_str=lookup_function_str,
-			lookup_function_stroke=lookup_function_stroke,
-			Dictionary=Dictionary,
-			SingleDictionary=SingleDictionary,
+			SingleDictionary=functools.partial(SingleDictionary, stroke_type),
+			stroke=functools.partial(stroke, stroke_type),
+			translation=functools.partial(translation, stroke_type),
 			)
 
 def get_context_from_system(system: Any)->Context:
